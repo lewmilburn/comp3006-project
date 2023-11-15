af19d33016afdba5d83157979f2a54663a392f54
lewmilburn <lewiscmilburn@gmail.com>
2023-11-15 14:52:07 +0000

    Updated tests

--- app/test/test.js
+++ app/test/test.js
@@ -4,14 +4,21 @@ const {equal} = require("assert");
 
 chai.use(chaiHttp)
 
-describe('Test', () => {
-   describe('Get homepage',() => {
+describe('Tests', () => {
+   describe('Server response',() => {
        it('Should have HTTP status 200', () => {
-          chai.request(`http://localhost`)
-              .get('/')
-              .end((err, res) => {
-                  equal(res.statusCode === 200,true,"Server does not have status 200");
-              });
+           chai.request(`http://localhost`)
+               .get('/')
+               .end((err, res) => {
+                   equal(res.statusCode === 200,true,"Server does not have status 200");
+               });
+       });
+       it('Should not send x-powered-by header', () => {
+           chai.request(`http://localhost`)
+               .get('/')
+               .end((err, res) => {
+                   equal(res.headers['x-powered-by'],undefined,"Server sending x-powered-by");
+               });
        });
     });
 });
