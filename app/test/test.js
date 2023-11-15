f7c8687399ee4f37d15a7730d47d82abcc5a321a
lewmilburn <lewiscmilburn@gmail.com>
2023-11-15 14:48:57 +0000

    Added test for status 200

--- app/test/test.js
+++ app/test/test.js
@@ -1,8 +1,17 @@
-var assert = require('assert');
-describe('Test', function () {
-    describe('Test1()', function () {
-        it('An example text', function () {
-            assert.equal(1,1);
-        });
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+const {equal} = require("assert");
+
+chai.use(chaiHttp)
+
+describe('Test', () => {
+   describe('Get homepage',() => {
+       it('Should have HTTP status 200', () => {
+          chai.request(`http://localhost`)
+              .get('/')
+              .end((err, res) => {
+                  equal(res.statusCode === 200,true,"Server does not have status 200");
+              });
+       });
     });
 });
