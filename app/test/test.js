1b08c08df146b9a109fb869d783646177e5874b3
lewmilburn <lewiscmilburn@gmail.com>
2023-11-15 14:14:20 +0000

    Fix testing

---
+++ app/test/test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('Test', function () {
+    describe('Test1()', function () {
+        it('An example text', function () {
+            assert.equal(1,1);
+        });
+    });
+});
