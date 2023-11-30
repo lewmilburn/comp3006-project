a0e028a31ced6444db74b18bd67a002595dcab95
Lewis Milburn <lewiscmilburn@gmail.com>
2023-11-30 14:30:54 +0000

    [GET] From database

--- server/api/rooms.js
+++ server/api/rooms.js
@@ -1,6 +1,34 @@
-module.exports = function (server) {
+module.exports = function (server, database) {
     server.get("/api/rooms", function(request, response) {
         response.setHeader('Content-Type', 'application/json');
-        response.send(JSON.parse('[{"id":1, "price": "30"}]'));
+        getAllRooms(database).then(r => {
+            response.send(r);
+        });
     });
+}
+
+async function getAllRooms(client) {
+    try {
+        await client.connect();
+        const database = client.db("COMP3006Hotel");
+        const rooms = database.collection("rooms");
+
+        const query = {};
+        const options = {};
+
+        const cursor = rooms.find(query, options);
+        if ((await rooms.countDocuments(query)) === 0) {
+            console.log("No documents found!");
+        }
+
+        let arr = [];
+        let i = 0;
+        for await (const doc of cursor) {
+            arr[i] = doc;
+            i++;
+        }
+        return arr;
+    } finally {
+        await client.close();
+    }
 }
--- server/server.js
+++ server/server.js
@@ -1,22 +1,24 @@
 let serverPort = 8080;
 let webSocketPort = 6436;
 const dotenv = require("dotenv");
+const {resolve} = require("path");
 
-console.log("[STARTUP][1/4] Loading configuration...");
+console.log("[STARTUP][1/5] Loading configuration...");
 dotenv.config();
 
 // Start API
-console.log("[STARTUP][2/4] Starting API...");
+console.log("[STARTUP][2/5] Starting API Server...");
 let server = require('./startup/api.js')(serverPort);
 // Start WebSocket
-console.log("[STARTUP][3/4] Starting WebSocket...");
+console.log("[STARTUP][3/5] Starting WebSocket Server...");
 require('./startup/socket.js')(webSocketPort);
 // Start database
-console.log("[STARTUP][4/4] Starting Database...");
-require ('./startup/database.js')(process.env.DB_CONN_STRING);
-console.log("[STARTUP] Done");
+console.log("[STARTUP][4/5] Starting Database...");
+let database = require ('./startup/database.js')(process.env.DB_CONN_STRING);
 
 // Routing
+console.log("[STARTUP][5/5] Starting Router...");
 require('./api/status.js')(server);
-require('./api/rooms.js')(server);
+require('./api/rooms.js')(server, database);
 require('./api/bookings.js')(server);
+console.log("[STARTUP] Done");
