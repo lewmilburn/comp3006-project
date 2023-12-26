9263e269e0146878780351b8eb44d36c0d35cea4
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2023-12-26 13:23:46 +0000

    Updated test error messages

--- server/test.js
+++ server/test.js
@@ -15,16 +15,16 @@ describe('Tests', () => {
             equal(escapeTest('Test! "'),'Test! &quot;',"Not escaping quotations");
         });
         it('Should escape less than symbol', () => {
-            equal(escapeTest('Test! <'),'Test! &lt;',"Not escaping quotations");
+            equal(escapeTest('Test! <'),'Test! &lt;',"Not escaping less than symbol");
         });
         it('Should escape greater than symbol', () => {
-            equal(escapeTest('Test! >'),'Test! &gt;',"Not escaping quotations");
+            equal(escapeTest('Test! >'),'Test! &gt;',"Not escaping greater than symbol");
         });
         it('Should escape apostrophe symbol', () => {
-            equal(escapeTest("Test! '"),'Test! &#x27;',"Not escaping quotations");
+            equal(escapeTest("Test! '"),'Test! &#x27;',"Not escaping apostrophe symbol");
         });
         it('Should escape slash symbol', () => {
-            equal(escapeTest("Test! /"),'Test! &#x2F;',"Not escaping quotations");
+            equal(escapeTest("Test! /"),'Test! &#x2F;',"Not escaping slash symbol");
         });
     });
 });
