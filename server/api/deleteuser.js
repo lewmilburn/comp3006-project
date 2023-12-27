66870c1e7faf82f8ed85cdd166a7401849678718
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2023-12-27 15:55:59 +0000

    Throwing false responses.

--- server/api/deleteuser.js
+++ server/api/deleteuser.js
@@ -1,20 +1,13 @@
 module.exports = function (server, database) {
-    server.post("/api/deleteuser", function(request, response) {
+    server.post("/api/deleteuser", async function(request, response) {
         let id = request.body.id;
         let token = request.body.token;
 
         id = require('../functions/escape.js')(id)
 
-        let res = require('../functions/database/user_delete')(database,id,token);
+        require('../functions/database/user_delete')(database,id,token);
 
-        if (res) {
-            console.log('[API][200] /api/deleteuser');
-            response.status(200).send('Success');
-            response.send(JSON.parse('{"status":200}'));
-        } else {
-            console.log('[API][404] /api/deleteuser');
-            response.status(404).send('Not Found');
-            response.send(JSON.parse('{"status":404}'));
-        }
+        console.log('[API][200] /api/deleteuser');
+        response.status(200).send('Success');
     });
 }
