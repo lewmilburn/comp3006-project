a0b61ecd9c57f3b809caa6e26d32d6c37bbbc7f8
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2023-12-27 15:56:18 +0000

    Throwing false responses.

--- server/api/deleteuser.js
+++ server/api/deleteuser.js
@@ -5,9 +5,10 @@ module.exports = function (server, database) {
 
         id = require('../functions/escape.js')(id)
 
-        require('../functions/database/user_delete')(database,id,token);
-
-        console.log('[API][200] /api/deleteuser');
-        response.status(200).send('Success');
+        require('../functions/database/user_delete')(database, id, token).then(r  => {
+            console.log(r);
+            console.log('[API][200] /api/deleteuser');
+            response.status(200).send('Success');
+        });
     });
 }
