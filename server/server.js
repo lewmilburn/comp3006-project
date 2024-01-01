6d99c0c370c10a31ccef5b0598ae1d76f8617425
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2024-01-01 19:48:44 +0000

    Easier to read.

--- server/server.js
+++ server/server.js
@@ -16,19 +16,30 @@ console.log("[STARTUP][4/5] Starting Database...");
 let database = require ('./startup/database.js')(process.env.DB_CONN_STRING);
 
 // Routing
-console.log("[STARTUP][5/5] Starting Router...");
+console.log("[STARTUP][5/5] Starting Router (1/4 - Status)");
 require('./api/status.js')(server);
+
+console.log("[STARTUP][5/5] Starting Router (2/4 - Rooms)");
 require('./api/rooms_retrieve.js')(server, database);
 require('./api/room_create.js')(server, database);
 require('./api/room_retrieve.js')(server, database);
 require('./api/room_update.js')(server, database);
 require('./api/room_delete.js')(server, database);
+
+console.log("[STARTUP][5/5] Starting Router (3/4 - Bookings)");
 require('./api/booking_create.js')(server, database);
 require('./api/booking_retrieve.js')(server, database);
+require('./api/booking_update.js')(server, database);
+require('./api/booking_delete.js')(server, database);
+
+console.log("[STARTUP][5/5] Starting Router (4/4 - Users)");
 require('./api/user_create.js')(server, database);
 require('./api/user_retrieve.js')(server, database);
 require('./api/user_update.js')(server, database);
 require('./api/user_delete.js')(server, database);
+
+console.log("[STARTUP][5/5] Starting Router (Done)");
+
 console.log("[STARTUP] Done");
 
 module.exports = server;
