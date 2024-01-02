a081827510c615337a2a2710825cf58fc635a623
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2024-01-02 23:09:12 +0000

    Fix crash

--- server/test.js
+++ server/test.js
@@ -39,7 +39,7 @@ describe('Tests', () => {
             let testRoomNumber, testRoomId;
             it('Add room', async () => {
                 console.log('[TEST][6/18] Running...')
-                let database = await require('./startup/database')(connString);
+                let database = await require('./startup/testDatabase')(connString);
                 await require('./functions/database/room_create')(await database, 'Test', 'T1', '1', '1', '1', '1', '1').then(r => {
                     let res = r !== null;
                     console.log('[TEST][6/18] Result: '+r);
@@ -49,7 +49,7 @@ describe('Tests', () => {
             });
             it('Retrieve rooms', async () => {
                 console.log('[TEST][7/18] Running...')
-                let database = await require('./startup/database')(connString);
+                let database = await require('./startup/testDatabase')(connString);
                 await require('./functions/database/rooms_retrieve')(await database).then(r => {
                     let res = r !== null;
                     console.log('[TEST][7/18] Result: '+r);
@@ -59,7 +59,7 @@ describe('Tests', () => {
             });
             it('Retrieve test room', async () => {
                 console.log('[TEST][8/18] Running...')
-                let database = await require('./startup/database')(connString);
+                let database = await require('./startup/testDatabase')(connString);
                 await require('./functions/database/room_retrieve')(await database, 'T1').then(r => {
                     testRoomNumber = r[0].room_number;
                     testRoomId = r[0]._id+'';
@@ -71,7 +71,7 @@ describe('Tests', () => {
             });
             it('Update room', async () => {
                 console.log('[TEST][9/18] Running...')
-                let database = await require('./startup/database')(connString);
+                let database = await require('./startup/testDatabase')(connString);
                 await require('./functions/database/room_update')(await database, testRoomId, 'Test2', 'T1', '2', '2', '2', '2', '2').then(r => {
                     let res = r !== null;
                     console.log('[TEST][9/18] Result: '+r);
@@ -81,7 +81,7 @@ describe('Tests', () => {
             });
             it('Delete test room', async () => {
                 console.log('[TEST][10/18] Running...')
-                let database = await require('./startup/database')(connString);
+                let database = await require('./startup/testDatabase')(connString);
                 await require('./functions/database/room_delete')(await database, testRoomNumber).then(r => {
                     let res = r !== null;
                     console.log('[TEST][10/18] Result: '+r);
@@ -94,7 +94,7 @@ describe('Tests', () => {
             let testBookingNumber;
             it('Add booking', async () => {
                 console.log('[TEST][11/18] Running...')
-                let database = await require('./startup/database')(connString);
+                let database = await require('./startup/testDatabase')(connString);
                 await require('./functions/database/booking_create')(await database, 'TEST', 'TEST', '0000-00-00', '0000-00-00').then(r => {
                     let res = r !== null;
                     console.log('[TEST][11/18] Result: '+r);
@@ -104,7 +104,7 @@ describe('Tests', () => {
             });
             it('Retrieve booking', async () => {
                 console.log('[TEST][12/18] Running...')
-                let database = await require('./startup/database')(connString);
+                let database = await require('./startup/testDatabase')(connString);
                 await require('./functions/database/booking_retrieve')(await database, 'TEST', 'TEST').then(r => {
                     testBookingNumber = r[0]._id+'';
                     let res = r !== null;
@@ -115,7 +115,7 @@ describe('Tests', () => {
             });
             it('Update test booking', async () => {
                 console.log('[TEST][13/18] Running...')
-                let database = await require('./startup/database')(connString);
+                let database = await require('./startup/testDatabase')(connString);
                 await require('./functions/database/booking_update')(await database, testBookingNumber, '1000-00-00', '1000-00-00').then(r => {
                     let res = r !== null;
                     console.log('[TEST][13/18] Result: '+r);
@@ -125,7 +125,7 @@ describe('Tests', () => {
             });
             it('Delete test booking', async () => {
                 console.log('[TEST][14/18] Running...')
-                let database = await require('./startup/database')(connString);
+                let database = await require('./startup/testDatabase')(connString);
                 await require('./functions/database/booking_delete')(await database, testBookingNumber).then(r => {
                     let res = r !== null;
                     console.log('[TEST][14/18] Result: '+r);
@@ -138,7 +138,7 @@ describe('Tests', () => {
             let testUserId;
             it('Add user', async () => {
                 console.log('[TEST][15/18] Running...')
-                let database = await require('./startup/database')(connString);
+                let database = await require('./startup/testDatabase')(connString);
                 await require('./functions/database/user_create')(await database, 'test@test.com', 'test', 'John Doe', 'TOKEN').then(r => {
                     let res = r !== null;
                     console.log('[TEST][15/18] Result: '+r);
@@ -148,7 +148,7 @@ describe('Tests', () => {
             });
             it('Retrieve user', async () => {
                 console.log('[TEST][16/18] Running...')
-                let database = await require('./startup/database')(connString);
+                let database = await require('./startup/testDatabase')(connString);
                 await require('./functions/database/user_retrieve')(await database, 'test@test.com').then(r => {
                     let res = r !== null;
                     testUserId = r._id+'';
@@ -159,7 +159,7 @@ describe('Tests', () => {
             });
             it('Update user', async () => {
                 console.log('[TEST][17/18] Running...')
-                let database = await require('./startup/database')(connString);
+                let database = await require('./startup/testDatabase')(connString);
                 await require('./functions/database/user_update')(await database, testUserId, 'test@test.edu', 'AnotherTest', 'Jane Doe', 'TOKEN2!').then(r => {
                     let res = r !== null;
                     console.log('[TEST][17/18] Result: '+r);
@@ -169,7 +169,7 @@ describe('Tests', () => {
             });
             it('Delete user', async () => {
                 console.log('[TEST][18/18] Running...')
-                let database = await require('./startup/database')(connString);
+                let database = await require('./startup/testDatabase')(connString);
                 await require('./functions/database/user_delete')(await database, testUserId, 'TOKEN2!').then(r => {
                     let res = r !== null;
                     console.log('[TEST][18/18] Result: '+r);
