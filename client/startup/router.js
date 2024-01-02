16c93ec87274b8e909d2ad83e863130fd137ef67
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2024-01-02 15:11:37 +0000

    Reducing duplicate code.

--- client/startup/router.js
+++ client/startup/router.js
@@ -1,32 +1,27 @@
 module.exports = function (client) {
-    client.get("/", function(request, response) {
-        response.render("homepage");
-    });
     client.get("/room/*", function(request, response) {
         response.render("room");
     });
-    client.get("/room", function(request, response) {
-        response.render("homepage");
-    });
-    client.get("/login", function(request, response) {
-        response.render("login");
-    });
-    client.get("/logout", function(request, response) {
-        response.render("logout");
-    });
-    client.get("/register", function(request, response) {
-        response.render("register");
-    });
-    client.get("/account", function(request, response) {
-        response.render("account");
-    });
-    client.get("/offline", function(request, response) {
-        response.render("offline");
-    });
-    client.get("/404", function(request, response) {
-        response.render("404");
-    });
-    client.get("/manage", function(request, response) {
-        response.render("manage");
-    });
+
+    router('', 'homepage');
+    router('room', 'homepage');
+    router('login');
+    router('logout');
+    router('register');
+    router('account');
+    router('offline');
+    router('404');
+    router('manage');
+
+    function router(route, display = 'false') {
+        if (display === 'false') {
+            display = route;
+        }
+
+        client.get('/'+route, function(request, response) {
+            response.render(display);
+        });
+
+        console.log('[STARTUP][2/3] Router bound "/'+route+'" to "'+display+'.ejs"');
+    }
 }
