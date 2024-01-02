e90c9bc03350b655cedc732e81d419bce31af325
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2024-01-02 20:19:11 +0000

    Updated room tests.

--- server/test.js
+++ server/test.js
@@ -15,66 +15,66 @@ describe('Tests', () => {
     describe('Security tests',() => {
         it('Should escape quotations', () => {
             equal(escapeTest('Test! "'),'Test! &quot;',"Not escaping quotations");
-            console.log('[TEST][1/] Done')
+            console.log('[TEST][1/9] Done')
         });
         it('Should escape less than symbol', () => {
             equal(escapeTest('Test! <'),'Test! &lt;',"Not escaping less than symbol");
-            console.log('[TEST][2/] Done')
+            console.log('[TEST][2/9] Done')
         });
         it('Should escape greater than symbol', () => {
             equal(escapeTest('Test! >'),'Test! &gt;',"Not escaping greater than symbol");
-            console.log('[TEST][3/] Done')
+            console.log('[TEST][3/9] Done')
         });
         it('Should escape apostrophe symbol', () => {
             equal(escapeTest("Test! '"),'Test! &#x27;',"Not escaping apostrophe symbol");
-            console.log('[TEST][4/] Done')
+            console.log('[TEST][4/9] Done')
         });
         it('Should escape slash symbol', () => {
             equal(escapeTest("Test! /"),'Test! &#x2F;',"Not escaping slash symbol");
-            console.log('[TEST][5/] Done')
+            console.log('[TEST][5/9] Done')
         });
     });
     describe('Database tests',() => {
         describe('Room', () => {
             let testRoomNumber;
             it('Add room', async () => {
-                console.log('[TEST][6/] Running...')
+                console.log('[TEST][6/9] Running...')
                 let database = await require('./startup/database')(connString);
                 await require('./functions/database/room_create')(await database, 'Test', 'T1', '1', '1', '1', '1', '1').then(r => {
                     let res = r !== null;
-                    console.log('[TEST][6/] '+r);
-                    console.log('[TEST][6/] Done');
+                    console.log('[TEST][6/9] Result: '+r);
+                    console.log('[TEST][6/9] Done');
                     equal(res, true, 'Could not add room.');
                 });
             });
             it('Retrieve rooms', async () => {
-                console.log('[TEST][7/] Running...')
+                console.log('[TEST][7/9] Running...')
                 let database = await require('./startup/database')(connString);
                 await require('./functions/database/rooms_retrieve')(await database).then(r => {
                     let res = r !== null;
-                    console.log('[TEST][7/] '+r);
-                    console.log('[TEST][7/] Done');
+                    console.log('[TEST][7/9] Result: '+r);
+                    console.log('[TEST][7/9] Done');
                     equal(res, true, 'No rooms in database.');
                 });
             });
             it('Retrieve test room', async () => {
-                console.log('[TEST][8/] Running...')
+                console.log('[TEST][8/9] Running...')
                 let database = await require('./startup/database')(connString);
                 await require('./functions/database/room_retrieve')(await database, 'T1').then(r => {
                     testRoomNumber = r[0].room_number;
                     let res = r !== null;
-                    console.log('[TEST][8/] '+r);
-                    console.log('[TEST][8/] Done');
+                    console.log('[TEST][8/9] Result: '+r);
+                    console.log('[TEST][8/9] Done');
                     equal(res, true, 'Room not in database.');
                 });
             });
             it('Delete test room', async () => {
-                console.log('[TEST][9/] Running...')
+                console.log('[TEST][9/9] Running...')
                 let database = await require('./startup/database')(connString);
                 await require('./functions/database/room_delete')(await database, testRoomNumber).then(r => {
                     let res = r !== null;
-                    console.log('[TEST][9/] '+r);
-                    console.log('[TEST][9/] Done');
+                    console.log('[TEST][9/9] Result: '+r);
+                    console.log('[TEST][9/9] Done');
                     equal(res, true, 'Could not delete room.');
                 });
             });
