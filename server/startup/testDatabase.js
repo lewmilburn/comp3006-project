fe5267d96fc5afcf824ac27b607cf88e089026e0
Lewis Milburn <lewiscmilburn@gmail.com>
2024-01-05 18:57:39 +0000

    Update

--- server/startup/testDatabase.js
+++ server/startup/testDatabase.js
@@ -1,7 +1,6 @@
 module.exports = async function(connectionString) {
     const { MongoClient, ServerApiVersion } = require('mongodb');
 
-    // Create a MongoClient with a MongoClientOptions object to set the Stable API version
     const client = new MongoClient(connectionString, {
         serverApi: {
             version: ServerApiVersion.v1,
